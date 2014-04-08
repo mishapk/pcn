4ea4a956ec6d769a53f287ae866c68aaf5bb7682
mishapk <mishapk@ya.ru>
2014-04-08 11:50:08 +0300

    Fig n1

--- moc_notify.cpp
+++ moc_notify.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'notify.h'
 **
-** Created: Sun Mar 9 14:06:25 2014
+** Created: Tue Apr 8 11:48:40 2014
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
